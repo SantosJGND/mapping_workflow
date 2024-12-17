0100fccae1ca882b661665800fbc7274832d2c98
SantosJGND <dourado.jns@gmail.com>
2024-12-17 18:16:56 +0000

    sh generalied

--- map_references.sh
+++ map_references.sh
@@ -4,7 +4,7 @@ REF_DIR=$1
 FASTQ_DIR=$2
 OUTPUT_DIR=$3
 
-NEXTFLOW_DIRECTORY=/mnt/extra_disk_03_5TB/JOAO/TOOLS/mapping_workflow
+NEXTFLOW_DIRECTORY=/path/to/nextflow
 
 
 nextflow run $NEXTFLOW_DIRECTORY"/map_references_bash.nf" --references $REF_DIR --reads $FASTQ_DIR --output_dir $OUTPUT_DIR
